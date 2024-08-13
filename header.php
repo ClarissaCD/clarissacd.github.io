f0603a3dd249eee46c844768799bc0dd5f4fe4fc
ClarissaCD <99683778+ClarissaCD@users.noreply.github.com>
2024-08-12 20:34:11 -0500

    deleting index.html

---
+++ header.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<header>
+    <ul id="header-menu">
+      <li><a href="/#about">About</a></li>
+      <li>Design</li>
+        <ul class="sub-menu">
+        <li>Graphic</li>
+        <li>User Experience</li>
+        </ul>
+      <li>Development</li>
+    </ul>
+  </header>
+</html>
---
+++ pages/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+<title>Clarissa Duran</title>
+
+<link rel="stylesheet" href="CSS/main.css" type="text/css">
+</head>
+
+<body>
+<?php
+include('header.php');
+?>
+  
+  <main>
+    <section>
+      <div>
+        <span id="about"></span>
+        <h2>About Me</h2>      
+      </div>
+    </section>
+  </main>
+  
+<?php
+include('footer.php');
+?>
+</body>
+
+</html>
